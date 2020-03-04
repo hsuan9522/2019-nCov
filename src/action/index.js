60608cece91aadef3560643fe430774c09f10d26
Hsuan Chang <hsuanchang@starlux-airlines.com>
2020-03-04 16:16:13 +0800

    rewrite async/await

--- src/action/index.js
+++ src/action/index.js
@@ -1,59 +1,55 @@
 import axios from 'axios';
 import zhCountryName from '../config/countryName.json';
 
-export const getCountryVirusData = ()=> {
-  return (dispatch) =>{
-    return axios.get('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true')
-      .then(res=>{
-        let tmpTotal = {
-          confirmedCount: 0,
-          curedCount: 0,
-          deadCount: 0,
-          updateTime: 0
-        };
-        const tmpData = res.data.features.map(el => {
-          tmpTotal.confirmedCount += el.attributes.Confirmed;
-          tmpTotal.curedCount += el.attributes.Recovered;
-          tmpTotal.deadCount += el.attributes.Deaths;
-          tmpTotal.updateTime =
-            el.attributes.Last_Update > tmpTotal.updateTime
-              ? el.attributes.Last_Update
-              : tmpTotal.updateTime;
-          return el.attributes;
-        });
-        dispatch({
-          type: "ADD_COUNTRY_INFECTED",
-          data: tmpData
-        });
-        dispatch({
-          type: "ADD_TOTAL_DATA",
-          data: tmpTotal,
-        })
-      })
+export const getCountryVirusData = () => async dispatch => {
+  try {
+    const res = await axios.get('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&outSR=102100&resultOffset=0&resultRecordCount=250&cacheHint=true')
+    let tmpTotal = {
+      confirmedCount: 0,
+      curedCount: 0,
+      deadCount: 0,
+      updateTime: 0
+    };
+    const tmpData = res.data.features.map(el => {
+      tmpTotal.confirmedCount += el.attributes.Confirmed;
+      tmpTotal.curedCount += el.attributes.Recovered;
+      tmpTotal.deadCount += el.attributes.Deaths;
+      tmpTotal.updateTime =
+        el.attributes.Last_Update > tmpTotal.updateTime
+          ? el.attributes.Last_Update
+          : tmpTotal.updateTime;
+      return el.attributes;
+    });
+    dispatch({
+      type: "ADD_COUNTRY_INFECTED",
+      data: tmpData
+    });
+    dispatch({
+      type: "ADD_TOTAL_DATA",
+      data: tmpTotal,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+};
+
+export const getCountryInfo = () => async dispatch => {
+  try {
+    const res = await axios.get('https://restcountries.eu/rest/v2/all');
+    const tmp = zhCountryName.map(el => {
+      let detail = res.data.find(e => e.alpha2Code === el.code);
+      if (detail) {
+        const { region, flag, subregion, alpha3Code } = detail;
+        return Object.assign(el, { region, flag, subregion, alpha3Code })
+      } else {
+        return el;
+      }
+    })
+    dispatch({
+      type: "ADD_COUNTRY_INFO",
+      data: tmp
+    })
+  } catch (err) {
+    console.log(err)
   }
 }
-
-export const getCountryInfo = ()=> {
-  return dispatch =>{
-    return axios.get('https://restcountries.eu/rest/v2/all')
-      .then(res=>{
-        const tmp = zhCountryName.map(el=>{
-          let detail = res.data.find(e => e.alpha2Code===el.code);
-          if(detail){
-            const { region, flag, subregion } = detail;
-            return Object.assign(el, { region, flag, subregion })
-          }else{
-            return el;
-          }
-        })
-        dispatch({ 
-          type: "ADD_COUNTRY_INFO",
-          data: tmp
-        })
-      })
-      .catch(err=>{
-      })
-  }
-}
-
-// export { getCountryVirusData }
--- src/components/State/index.js
+++ src/components/State/index.js
@@ -1,18 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 import _ from "lodash";
 
+import Region from "./Region"
+
 const State = () => {
   const countryInfected = useSelector(state=>state.countryInfected);
-  const countryName = useSelector(state=>state.countryInfo);
+  const countryName = useSelector(state => state.countryInfo);
+  const [dataRegion, setDataRegion] = useState([])
 
   useEffect(()=>{
-    console.log(_.groupBy(countryName,'subregion'))
-
-  })
+    // const data = countryInfected.map(el=>{
+    //   let info = countryName.find(e=>e.key === el.Country_Region );
+    //   if(info){
+    //     return Object.assign(el,info);
+    //   }else{
+    //     el.region = 'Other';
+    //     el.subregion = 'Other';
+    //     el.flag = null;
+    //     return el;
+    //   }
+    // })
+    // const dataByRegion = Object.values(data.reduce((acc, re)=>{
+    //   if(acc[re.region]){
+    //     acc[re.region]['Confirmed'] += re.Confirmed;
+    //     acc[re.region]['Deaths'] += re.Deaths;
+    //     acc[re.region]['Recovered'] += re.Recovered;
+    //   }else{
+    //     acc[re.region] = {
+    //       region: re.region,
+    //       Confirmed: re.Confirmed,
+    //       Deaths: re.Deaths,
+    //       Recovered: re.Recovered,
+    //     };
+    //   }
+    //   return acc;
+    // },{}))
+    console.log('countryInfected', countryInfected);
+    console.log('countryName', countryName);
+    // setDataRegion(dataByRegion);
+  }, [countryInfected, countryName])
 
   return (
     <div>
+      {dataRegion &&
+        <Region data={dataRegion} />
+      }
+
       <div>123</div>
     </div>
   )
