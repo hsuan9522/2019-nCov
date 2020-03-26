b3e436fcafcffcf79b85f2202b9ae0c567d7b9d1
Hsuan Chang <hsuanchang@starlux-airlines.com>
2020-03-26 11:30:00 +0800

    Update chart and countryName

--- src/components/Country/index.js
+++ src/components/Country/index.js
@@ -6,11 +6,7 @@ const CountryChart = props => {
   const data = useSelector(state => state.countryInfected);
   const countryName = useSelector(state => state.countryInfo);
   if(!data || !countryName) return (<div>There is no Data!</div>)
-  const withoutChina = data.filter(
-    el => { 
-      return el.Country_Region !== "China" && el.Confirmed!==0
-    }
-  );
+  const limit = 5000;
   const mapTitle = {
     Confirmed: "確診人數",
     Recovered: "治癒人數",
@@ -22,10 +18,10 @@ const CountryChart = props => {
   return (
       <div>
         <h3 className="h3-title">各國人數狀態</h3>
-        <div style={{ height: "2000px" }}>
+        <div style={{ height: "2500px" }}>
           <ResponsiveBar
             layout="horizontal"
-            data={withoutChina}
+            data={data.filter(el => el.Confirmed < limit)}
             keys={keys}
             indexBy="Country_Region"
             margin={{ top: 5, right: 10, bottom: 50, left: 65 }}
@@ -54,10 +50,10 @@ const CountryChart = props => {
             )}
           />
         </div>
-        <div style={{ height: "85px" }}>
+        <div style={{ height: "300px" }}>
           <ResponsiveBar
             layout="horizontal"
-            data={data.filter(el => el.Country_Region === "China")}
+            data={data.filter(el => el.Confirmed >= limit)}
             keys={keys}
             indexBy="Country_Region"
             margin={{ top: 5, right: 10, bottom: 50, left: 65 }}
@@ -97,7 +93,7 @@ const CountryChart = props => {
         </div>
         <div style={{ textAlign: "right" }}>
           <small>*各國疫情人數持續增加，因此有些國家增加數量過大，會導致較少感染人數的國家圖表顯示不出來。</small>
-          <small>另外中國與其他國家人數差異過大，獨立為一圖表。</small>
+          <small>另外分為上下部份，下部分為確診人數超過5000人次。</small>
         </div>
       </div>
   )
