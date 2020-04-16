dd77805626aecd52bb6da760577d2c15d680e014
Hsuan Chang <hsuanchang@starlux-airlines.com>
2020-04-16 16:40:06 +0800

    Updates

---
+++ precache-manifest.029292291a4f20a03a184129f3002d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61c9362754e234990f6058483fa6479",
+    "url": "/2019-nCov/index.html"
+  },
+  {
+    "revision": "8e370c087fedc11a601e",
+    "url": "/2019-nCov/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "f55e90c0b24c6e616293",
+    "url": "/2019-nCov/static/css/main.6e15fbe6.chunk.css"
+  },
+  {
+    "revision": "8e370c087fedc11a601e",
+    "url": "/2019-nCov/static/js/2.23cfe44b.chunk.js"
+  },
+  {
+    "revision": "f55e90c0b24c6e616293",
+    "url": "/2019-nCov/static/js/main.5babc006.chunk.js"
+  },
+  {
+    "revision": "61cf044b66091396e016",
+    "url": "/2019-nCov/static/js/runtime~main.f81b73c2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2019-nCov/precache-manifest.f32d22e25b4f56f3a1bac255b4905515.js"
+  "/2019-nCov/precache-manifest.029292291a4f20a03a184129f3002d58.js"
 );
 
 self.addEventListener('message', (event) => {
