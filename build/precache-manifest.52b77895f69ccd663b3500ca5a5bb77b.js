08be7a4dd1b9380478fd00b26e7417a0ad76c23a
Hsuan Chang <hsuanchang@starlux-airlines.com>
2020-06-12 15:24:50 +0800

    build

---
+++ build/precache-manifest.52b77895f69ccd663b3500ca5a5bb77b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d2a8b807ad4c8178f4691c7f73d6a7",
+    "url": "/2019-nCov/index.html"
+  },
+  {
+    "revision": "9146615d9e0bddd77029",
+    "url": "/2019-nCov/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "52bbcc37cae2d15bbeaa",
+    "url": "/2019-nCov/static/css/main.6e15fbe6.chunk.css"
+  },
+  {
+    "revision": "9146615d9e0bddd77029",
+    "url": "/2019-nCov/static/js/2.7d84e5f2.chunk.js"
+  },
+  {
+    "revision": "52bbcc37cae2d15bbeaa",
+    "url": "/2019-nCov/static/js/main.524c4828.chunk.js"
+  },
+  {
+    "revision": "61cf044b66091396e016",
+    "url": "/2019-nCov/static/js/runtime~main.f81b73c2.js"
+  }
+]);
