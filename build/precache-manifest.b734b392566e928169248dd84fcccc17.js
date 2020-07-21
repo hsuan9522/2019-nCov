84069805e77794e677d7e90e611895205564de47
Hsuan Chang <jh558125@gmail.com>
2020-07-21 16:01:24 +0800

    build

---
+++ build/precache-manifest.b734b392566e928169248dd84fcccc17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f6ca42e493cc8d6576509e20357c89",
+    "url": "/2019-nCov/index.html"
+  },
+  {
+    "revision": "ee9d4624b004d9e94c49",
+    "url": "/2019-nCov/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "a190f83740210c299846",
+    "url": "/2019-nCov/static/css/main.8e6d663a.chunk.css"
+  },
+  {
+    "revision": "ee9d4624b004d9e94c49",
+    "url": "/2019-nCov/static/js/2.d23e6b7e.chunk.js"
+  },
+  {
+    "revision": "a190f83740210c299846",
+    "url": "/2019-nCov/static/js/main.1138dfaf.chunk.js"
+  },
+  {
+    "revision": "61cf044b66091396e016",
+    "url": "/2019-nCov/static/js/runtime~main.f81b73c2.js"
+  }
+]);
