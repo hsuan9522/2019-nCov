9ec26308eed196309d4f5a9da1e5dda711cc0475
Hsuan <jh558125@gmail.com>
2021-01-18 14:47:49 +0800

    Updates

---
+++ precache-manifest.68dacb3793bd6f4442540bb72db4ed8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e960a4b18cba299a55270dbf6405c7",
+    "url": "/2019-nCov/index.html"
+  },
+  {
+    "revision": "c30f5cf6542b3656b6d0",
+    "url": "/2019-nCov/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "5d4d2cc83b5bce5ec2f8",
+    "url": "/2019-nCov/static/css/main.b49426c3.chunk.css"
+  },
+  {
+    "revision": "c30f5cf6542b3656b6d0",
+    "url": "/2019-nCov/static/js/2.6af47334.chunk.js"
+  },
+  {
+    "revision": "5d4d2cc83b5bce5ec2f8",
+    "url": "/2019-nCov/static/js/main.2350c0b8.chunk.js"
+  },
+  {
+    "revision": "61cf044b66091396e016",
+    "url": "/2019-nCov/static/js/runtime~main.f81b73c2.js"
+  }
+]);
